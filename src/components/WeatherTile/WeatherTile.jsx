d71d5da8492506d2f352a993c406f99c838526bc
Jennifer Bowser <jduffbowser@gmail.com>
2024-07-05 11:27:21 -0700

    updates to WeatherTile

--- src/components/WeatherTile/WeatherTile.jsx
+++ src/components/WeatherTile/WeatherTile.jsx
@@ -2,23 +2,6 @@ import getCondition from "../../util/conditions";
 import "./WeatherTile.css";
 
 export default function WeatherTile({ id }) {
-    // return (
-    //     getConditionText(id) &&
-    //     <div className="WeatherTile" style={{ backgroundColor: getConditionColor(id) }}>
-    //         <img src={getConditionIcon(id)} />
-    //         <p>{getConditionText(id)}</p>
-    //     </div>
-    // )
-
-    // const condition = getCondition(id);
-    // return (
-    //     condition &&
-    //     <div className="WeatherTile" style={{ backgroundColor: condition.color.day }}>
-    //         <img src={condition.icon.day} />
-    //         <p>{condition.text}</p>
-    //     </div>
-    // )
-
     const condition = getCondition(id);
     const tiles = [];
 
@@ -26,7 +9,7 @@ export default function WeatherTile({ id }) {
         tiles.push(
             <div className="WeatherTile" style={{ backgroundColor: condition.getColor() }} key={`${id}-day`}>
                 <img src={condition.getIcon()} />
-                <p>{condition.getText()}</p>
+                <p style={{ color: condition.getTextColor() }}>It's <span class="Bold">52&deg;</span> {condition.getLinkingWord()} <span class="Bold">{condition.getText()}</span></p>
             </div>
         );
     }
@@ -35,7 +18,7 @@ export default function WeatherTile({ id }) {
         tiles.push(
             <div className="WeatherTile" style={{ backgroundColor: condition.getColor(!condition.IS_DAY) }} key={`${id}-night`}>
                 <img src={condition.getIcon(!condition.IS_DAY)} />
-                <p style={{ color: condition.getTextColor(!condition.IS_DAY) }}>{condition.getText()}</p>
+                <p style={{ color: condition.getTextColor(!condition.IS_DAY) }}>It's <span class="Bold">52&deg;</span> {condition.getLinkingWord()} <span class="Bold">{condition.getText()}</span></p>
             </div>
         );
     }
