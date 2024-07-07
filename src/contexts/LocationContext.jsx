b5937bd6f66134ed19d771c11a024b20525dbb80
jennifer.bowser <jduffbowser@gmail.com>
2024-07-07 09:33:46 -0700

    gets location key from LocationContext

--- src/contexts/LocationContext.jsx
+++ src/contexts/LocationContext.jsx
@@ -1,24 +1,27 @@
-// import secrets from "../../secrets.js";
+import secrets from "../../secrets.js";
 import { createContext } from "react";
 
-const LocationContext = createContext("Puyallup");
 
-// export default async function getLocationKey(location) {
-//     const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${secrets["api_key"]}&q=${location}`;
-//     try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//             // throw new Error(`Response status: ${response.status}`);
-//             console.log(`Response status: ${response.status}`);
-//         }
+async function getLocationKey(location) {
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${secrets["api_key"]}&q=${location}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            // throw new Error(`Response status: ${response.status}`);
+            console.log(`Response status: ${response.status}`);
+        }
 
-//         const json = await response.json();
+        const json = await response.json();
 
-//         return json;
-//     }
-//     catch {
-//         console.log("Oh no, error!");
-//     }
-// }
+        return json[0]["Key"];
+    }
+    catch {
+        console.log("Oh no, error!");
+    }
+}
+
+const locationKey = await getLocationKey("Puyallup");
+
+const LocationContext = createContext(locationKey);
 
 export default LocationContext;
