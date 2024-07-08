8676586505f4274424f077ca893ec6a06f043d8a
jennifer.bowser <jduffbowser@gmail.com>
2024-07-07 18:30:58 -0700

    WIP - using context to call second API endpoint

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,33 +1,41 @@
 import "./CurrentWeather.css";
 import secrets from "../../../secrets";
+import getCondition from "../../util/getCondition";
 import { useContext, useEffect, useState } from "react";
 import LocationContext, { FetchLocationCode } from "../../contexts/LocationContext";
 
 export default function CurrentWeather() {
-    // const locationCode = useContext(LocationContext);
     const locationCode = FetchLocationCode();
-    const [conditionCode, setConditionCode] = useState(null);
+    const [condition, setCondition] = useState(null);
 
     console.log(locationCode);
 
-    // useEffect(() => {
-    //     async function getCurrentCondition(locationCode) {
-    //         const url = `https://dataservice.accuweather.com/currentconditions/v1/${locationCode}?apikey=${secrets["api_key"]}`;
-    //         try {
-    //             const response = await fetch(url);
-    //             if (!response.ok) {
-    //                 // throw new Error(`Response status: ${response.status}`);
-    //                 console.log(`Response status: ${response.status}`);
-    //             }
+    useEffect(() => {
+        async function getCurrentCondition(locationCode) {
+            console.log(locationCode);
+            const url = `https://dataservice.accuweather.com/currentconditions/v1/${locationCode}?apikey=${secrets["api_key"]}`;
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    // throw new Error(`Response status: ${response.status}`);
+                    console.log(`Response status: ${response.status}`);
+                }
 
-    //             const json = await response.json();
-    //             setConditionCode(json[0]);
-    //         }
-    //         catch {
-    //             console.log("Oh no, error!");
-    //         }
-    //     }
+                const json = await response.json();
+                const conditionCode = json["WeatherIcon"];
+                // setConditionCode(conditionCode);
 
-    //     const currentCondition = getCurrentCondition(locationCode);
-    // }, [locationCode]);
+                const currentCondition = getCondition(conditionCode);
+                setCondition(currentCondition);
+            }
+            catch {
+                console.log("Oh no, error!");
+            }
+        }
+
+        getCurrentCondition();
+
+    }, [locationCode]);
+
+    console.log(condition);
 }
