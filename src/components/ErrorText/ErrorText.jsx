8b35b441ad141838bc5f50ccb3696b9e1b64e7e6
jennifer.bowser <jduffbowser@gmail.com>
2024-07-10 19:06:45 -0700

    adds ErrorText and error_icon

---
+++ src/components/ErrorText/ErrorText.jsx
@@ -0,0 +1,17 @@
+export default function ErrorText({ locationCode }) {
+    const getText = (locationCode) => {
+        let content = "";
+        if (locationCode) {
+            content = "Hmm, looks like we're having issues. Try again later!"
+        }
+        else {
+            content = "Tell us where you're at"
+        }
+
+        return content;
+    }
+
+    return (
+        <p>{getText(locationCode)}</p>
+    )
+}
