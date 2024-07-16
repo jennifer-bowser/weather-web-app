5ea6f96b9a2865be264d6086c812be3a6386fdb9
Jennifer Bowser <jduffbowser@gmail.com>
2024-07-16 08:59:50 -0700

    updates ConditionText styling for non-weather words

--- src/components/atoms/ConditionText/ConditionText.jsx
+++ src/components/atoms/ConditionText/ConditionText.jsx
@@ -3,9 +3,9 @@ import "./ConditionText.css";
 export default function ConditionText({timeframe, condition}){
     return(
         <p className="ConditionText">
-            {timeframe}&nbsp;
+            <span className="ConditionText-Small">{timeframe}&nbsp;</span>
             <span className="Bold">{condition.getTemp()}</span>
-            <span className="ConditionText-LinkingWord">{condition.getLinkingWord()}</span>
+            <span className="ConditionText-LinkingWord ConditionText-Small">{condition.getLinkingWord()}</span>
             <span className="Bold">{condition.getText()}</span>
         </p>
     )
